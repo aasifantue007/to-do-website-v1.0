cbb69fb90100e45497b0131f5077da2d7b0604a4
Aasif Antue <aasifantue@Antues-Mac-Mini.local>
2023-04-08 17:54:30 +0600

    date time added

--- script.js
+++ script.js
@@ -32,3 +32,12 @@ const addToDo = (item) => {
 
     toDoBox.appendChild(listItem)
 }
+
+// Show Date //
+
+let time = document.getElementById("time");
+
+setInterval(() =>{
+    let d = new Date();
+    time.innerHTML = d.toLocaleString();
+},1000)
